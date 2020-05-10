6878743eebb7f86eed87572b7c22c63c18006d65
gaaoge <gaaoge@163.com>
2020-05-10 18:51:22 +0800

    更新flutter初始项目框架。

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # playground
 
-A new Flutter project.
+A new Flutter application.
 
 ## Getting Started
 
---
+++ android/app/src/main/kotlin/com/gg/playground/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gg.playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
