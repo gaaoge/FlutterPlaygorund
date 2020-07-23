5c9d15a8299d0c1badfabaf8320d028b0bd4be96
gaoge <gaoge@corp.netease.com>
2020-07-23 15:51:48 +0800

    格式化代码。

--- lib/pages/group/widgets/PaddingDemo.dart
+++ lib/pages/group/widgets/PaddingDemo.dart
@@ -6,49 +6,51 @@ class PaddingDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return BasePanel(
       title: 'Padding填充',
-      child: Column(children: <Widget>[
-        DecoratedBox(
-          decoration: BoxDecoration(
-            color: Colors.red,
+      child: Column(
+        children: <Widget>[
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text('Hello world'),
+            ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text('Hello world'),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text('Hello world'),
+            ),
           ),
-        ),
-        DecoratedBox(
-          decoration: BoxDecoration(
-            color: Colors.green,
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text('Hello world'),
+            ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: Text('Hello world'),
-          ),
-        ),
-        DecoratedBox(
-          decoration: BoxDecoration(
-            color: Colors.blue,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Text('Hello world'),
-          ),
-        ),
-        DecoratedBox(
-          decoration: BoxDecoration(
-            color: Colors.black,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 0, 20, 10),
-            child: Text(
-              'Hello world',
-              style: TextStyle(
-                color: Colors.white,
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 20, 10),
+              child: Text(
+                'Hello world',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
